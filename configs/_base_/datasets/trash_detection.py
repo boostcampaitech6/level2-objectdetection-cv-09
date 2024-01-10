8975907184485ff1e8e01fcf0409415faf13e22e
ohkingtaek <mm0741@naver.com>
2024-01-10 23:42:34 +0900

    Feat: train / valid / test data config 추가하고 나머지 config 삭제
    Train Validation용 trash_detection.py라는 config와 test용 trash_detection_test.py라는 config 추가하고 나머지 불필요한 datasets/config들 삭제
    
    #23

---
+++ configs/_base_/datasets/trash_detection.py
@@ -0,0 +1,58 @@
+dataset_type = 'CocoDataset'
+data_root = '/data/ephemeral/home/dataset/'
+
+metainfo = {
+    'classes': ("General trash", "Paper", "Paper pack", "Metal", "Glass",
+                "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+}
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='PackDetInputs')
+]
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='train_fold_4.json',
+        data_prefix=dict(img=data_root),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='val_fold_4.json',
+        data_prefix=dict(img=data_root),
+        test_mode=True,
+        pipeline=test_pipeline))
+test_dataloader = val_dataloader
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'val_fold_4.json',
+    metric='bbox',
+    format_only=False)
+test_evaluator = val_evaluator
---
+++ configs/_base_/datasets/trash_detection_test.py
@@ -0,0 +1,79 @@
+dataset_type = 'CocoDataset'
+data_root = '/data/ephemeral/home/dataset/'
+
+metainfo = {
+    'classes': ("General trash", "Paper", "Paper pack", "Metal", "Glass",
+                "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+}
+
+custom_hooks = [dict(type='SubmissionHook')]
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackDetInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(type='PackDetInputs')
+]
+
+train_dataloader = dict(
+    batch_size=4,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='train_fold_4.json',
+        data_prefix=dict(img=data_root),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='val_fold_4.json',
+        data_prefix=dict(img=data_root),
+        test_mode=True,
+        pipeline=test_pipeline))
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root + 'val_fold_4.json',
+    metric='bbox',
+    format_only=False)
+
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        metainfo=metainfo,
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='test.json',
+        data_prefix=dict(img=data_root),
+        test_mode=True,
+        pipeline=test_pipeline))
+
+test_evaluator = dict(
+    type='CocoMetric',
+    metric='bbox',
+    format_only=True,
+    ann_file=data_root + 'test.json',
+    outfile_prefix='./work_dirs/trash/test')
