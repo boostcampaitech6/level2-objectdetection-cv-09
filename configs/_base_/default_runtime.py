5a4401ba58428ed174befc5b82055f366e44a1a4
ohkingtaek <mm0741@naver.com>
2024-01-18 15:20:26 +0900

    Feat: Wandb Visualization add

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -6,7 +6,12 @@ default_hooks = dict(
     param_scheduler=dict(type='ParamSchedulerHook'),
     checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=3),
     sampler_seed=dict(type='DistSamplerSeedHook'),
-    visualization=dict(type='DetVisualizationHook'))
+    # ema=dict(type='EMAHook', ema_type='StochasticWeightAverage'),
+    visualization=dict(
+        type='DetVisualizationHook',
+        draw=True,
+        interval=1000)
+)
 
 env_cfg = dict(
     cudnn_benchmark=False,
@@ -14,15 +19,14 @@ env_cfg = dict(
     dist_cfg=dict(backend='nccl'),
 )
 
-vis_backends = [
-    dict(type='LocalVisBackend'),
-    dict(
-        type='WandbVisBackend',
-        init_kwargs={
-            'project': 'recycle_detection',
-            'group': 'faster_rcnn_r50'
-        })
-]
+vis_backends = [dict(type='LocalVisBackend'),
+                dict(
+                    type='WandbVisBackend',
+                    init_kwargs={
+                        'project': 'trash_detection',
+                        'group': 'cascade_rcnn'
+                    })
+                ]
 
 visualizer = dict(
     type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
